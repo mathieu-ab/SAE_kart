e9df0c25109442327829ee142f924cf7a65ada79
mathiieu <98045481+mathkiler@users.noreply.github.com>
2025-01-14 14:13:27 +0100

    Update lunch-linux.sh

--- centralisation_interface/lunch-linux.sh
+++ centralisation_interface/lunch-linux.sh
@@ -1,1 +1,10 @@
-python3 interface_central.py
+#!/bin/bash
+
+# Chemin vers le script Python
+PYTHON_SCRIPT="interface_central.py"
+
+# Lancer X avec le script Python
+xinit python3 "$PYTHON_SCRIPT" -- :0
+
+# Une fois que le script Python se termine, arrêter X
+pkill X
