f5b791b5c41bf092263c6ad66c0c50925a2f25ca
mathiieu <98045481+mathkiler@users.noreply.github.com>
2025-01-15 15:33:45 +0100

    Update lunch-linux.sh

--- centralisation_interface/lunch-linux.sh
+++ centralisation_interface/lunch-linux.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Chemin vers les scripts Python
+# Chemins vers les scripts Python
 PYTHON_SCRIPT1="/home/kartuser/SAE_kart/centralisation_interface/interface_central.py"
 PYTHON_SCRIPT2="/home/kartuser/BMS/BMS_lireSOC.py"
 
-# Lancer le premier script avec X
-xinit /bin/bash -c "python3 '$PYTHON_SCRIPT1'; pkill X" -- :0 &
-
-# Lancer le deuxième script Python en arrière-plan
+# Lancer le deuxième script Python en arrière-plan et capturer son PID
 python3 "$PYTHON_SCRIPT2" &
+SCRIPT2_PID=$!
+
+# Lancer le premier script avec X
+xinit /bin/bash -c "python3 '$PYTHON_SCRIPT1'; pkill X" -- :0
+
+# Une fois que le script 1 se termine, arrêter le script 2
+kill $SCRIPT2_PID
