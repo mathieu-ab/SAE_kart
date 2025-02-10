19c9267689a620c9b320abc546b1a3a01bb7584f
HilarieMourambou <hilarie.mourambou@gmail.com>
2025-02-10 16:22:54 +0100

    Update testUart.py

--- capteur_ultrason/testUart.py
+++ capteur_ultrason/testUart.py
@@ -1,10 +1,13 @@
 import serial
 
-# Ouvrir le port série
+# Configuration du port série (remplace "/dev/serial0" par "/dev/ttyS0" si nécessaire)
 ser = serial.Serial('/dev/serial0', 9600, timeout=1)
 ser.flush()
 
 while True:
     if ser.in_waiting > 0:
-        message = ser.readline().decode('utf-8').strip()
-        print("Reçu de l'Arduino :", message)
+        try:
+            distance = ser.readline().decode('utf-8' ,errors='ignore').strip()
+            print(f"Distance reçue : {distance} cm")
+        except UnicodeDecodeError:
+            print("Erreur de décodage, données corrompues")
