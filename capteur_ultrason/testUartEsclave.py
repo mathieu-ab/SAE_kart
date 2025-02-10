69b7c0715570b14383b4b1a24e02134212a4c35d
HilarieMourambou <hilarie.mourambou@gmail.com>
2025-02-10 17:09:09 +0100

    Create testUartEsclave.py

---
+++ capteur_ultrason/testUartEsclave.py
@@ -0,0 +1,12 @@
+import serial
+
+ser = serial.Serial('/dev/serial0', 9600, timeout=1)
+ser.flush()
+
+while True:
+    if ser.in_waiting > 0:
+        try:
+            distance = ser.readline().decode('utf-8' ,errors='ignore' ).strip()
+            print(f"Distance reçue : {distance} cm")
+        except UnicodeDecodeError:
+            print("Erreur de décodage, données corrompues")
