d5445cbfc416078fd4024d347f419f382aa94667
mathiieu <98045481+mathieu-ab@users.noreply.github.com>
2025-02-14 10:59:16 +0100

    Update test_camera.py

--- centralisation_interface/script_test/test_camera.py
+++ centralisation_interface/script_test/test_camera.py
@@ -1,9 +1,16 @@
 import time
 import cv2
 cap = cv2.VideoCapture(0)
-time.sleep(1)  # Délai de 1 seconde pour s'assurer que la caméra est prête
+i = 0
 ret, frame = cap.read()
-print(ret, frame, cap)
+while frame == None :
+    ret, frame = cap.read()
+    print(frame)
+    time.sleep(0.5)
+    if i == 20 :
+        break
+    else :
+        i+=1
 if ret:
     cv2.imshow("Frame", frame)
 else:
