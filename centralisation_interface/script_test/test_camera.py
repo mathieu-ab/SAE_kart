d38849c1c9296c484450b23afc991f7c0dd76b7e
mathiieu <98045481+mathieu-ab@users.noreply.github.com>
2025-02-14 10:54:52 +0100

    Update test_camera.py

--- centralisation_interface/script_test/test_camera.py
+++ centralisation_interface/script_test/test_camera.py
@@ -1,21 +1,11 @@
+import time
 import cv2
-
-# Ouvrir la caméra (index 0 pour la première caméra)
 cap = cv2.VideoCapture(0)
-
-# Lire une seule image
+time.sleep(1)  # Délai de 1 seconde pour s'assurer que la caméra est prête
 ret, frame = cap.read()
-
-# Vérifier si l'image a été capturée avec succès
 if ret:
-    # Afficher l'image
-    cv2.imshow("Captured Frame", frame)
-
-    # Attendre que l'utilisateur appuie sur une touche pour fermer la fenêtre
-    cv2.waitKey(0)
+    cv2.imshow("Frame", frame)
 else:
     print("Erreur : Impossible de récupérer une image.")
-
-# Libérer la caméra et fermer la fenêtre
 cap.release()
 cv2.destroyAllWindows()
