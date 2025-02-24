a05b9fbfc4bcbf43178a9529f8278b42a08809ea
Gawed <sdzy696@gmail.com>
2025-02-24 14:46:19 +0100

    Update test.py

--- GPS/test.py
+++ GPS/test.py
@@ -86,5 +86,5 @@ def display_map():
     pygame.quit()
 
 if __name__ == "__main__":
-    API_KEY = "AIzaSyDLvUcraTLttRBcvn728IaGCe_prAZK24Q"  # 你的 Google Maps API Key
+    API_KEY = "AIzaSyDLvUcraTLttRBcvn728IaGCe_prAZK24Q"  # Google Maps API Key
     display_map()
