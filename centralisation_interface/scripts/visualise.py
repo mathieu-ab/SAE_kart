10bdb429f3c865e0510119d85643835c897817db
smaaa3iinn <smaaa3iin@gmail.com>
2025-03-06 15:09:57 +0100

    three

--- centralisation_interface/scripts/visualise.py
+++ centralisation_interface/scripts/visualise.py
@@ -1,5 +1,9 @@
 import serial
-from setup_draw import setup_draw  # Import the UI setup
+import sys
+sys.path.append("/home/kartuser/SAE_kart/centralisation_interface/module_import.py")  # Replace with the actual path where `module_import.py` is
+
+from module_import import *  # Now try importing
+
 
 # Open serial connection to JeVois
 ser = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)
